cafad92148be790fd2544cab48a3daf56b6e8123
gitlG <1637885653@qq.com>
2024-02-20 01:48:08 +0800

    2023/11/13:自定义__repr__函数

--- test.py
+++ test.py
@@ -1,35 +1,20 @@
-#2023/11/11#序列化保存对象
-import dlib,pickle
-import numpy as np
-from dlib import vector
-from dlib import array as dlib_array
-from numpy import array
-
-x = np.arange(10)
-y = x ** 2
-np.savez("x_y-squared.npz", x_axis=x, y_axis=y)#保存为二进制文本
-
-load_xy = np.load("x_y-squared.npz")
-print(load_xy.files)#['x_axis', 'y_axis']
-x = load_xy["x_axis"]
-y = load_xy["y_axis"]
-
-dlib_array1 = dlib_array([1, 2, 3])
-print(type(dlib_array1))#<class 'dlib.dlib.array'>
-
-dlib_v1 = vector(dlib_array1)
-print(type(dlib_v1))#<class 'dlib.dlib.vector'>
-
-str_vector = dlib_v1.__repr__()#文本形式保存
-#输出：dlib.vector([1, 2, 3]) <class 'dlib.dlib.vector'>
-print(str_vector,type(eval(str_vector)))
-
-np_array = np.array([1,3,4])
-str_np_array = np_array.__repr__()
-
-np_array2 = eval(str_np_array)
-print(type(np_array2))#<class 'numpy.ndarray'>
-
-#序列化后以二进制形式保存
-pick = pickle.dumps(np_array)
-print(type(pick))
+2023/11/13#自定义__repr__函数
+class Person:
+    def __init__(self, name, age,*args,**kw,):
+        self.name = name
+        self.age = age
+        for key,value in kw.items():
+            self.__dict__[key] = value
+  
+    def test(self):
+        print("test")
+    def __repr__(self):
+        args = "" 
+        for key,value in self.__dict__.items():
+            if type(value) is str: 
+                value = "'" + value + "'" 
+            args = args + key + "=" + str(value) + ','
+        return f"Person({args})"
+    
+str_p = Person("mc",67,7,lin = "test",).__repr__()
+eval(str_p).test()#test
