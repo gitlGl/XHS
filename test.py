065e64e2a50ea88f959991c1cfd1531089b7b010
gitlG <1637885653@qq.com>
2024-02-20 13:52:03 +0800

    2023/10/19:主线程，守护线程，子线程，进程之间的退出顺序

--- test.py
+++ test.py
@@ -1,25 +1,56 @@
-#2023/11/13#函数柯里化例子
-def add(x, y, z):
-    return x + y + z
+#2023/10/19
+import threading
+import time
 
-def curry(fn):
-    def curried(*args):
-        if len(args) == 1:
-            return lambda y: lambda z: fn(args[0], y, z)
-        elif len(args) == 2:
-            return lambda z: fn(*args, z)
-        else:
-            return fn(*args)
-    return curried
+def worker():
+  count = 0
+  while True:
+    time.sleep(1)
+    count = count + 1 
+    print("我是线程Thread-1")
+    if count == 3:
+      print("Thread-1退出")
+      break
+  
+def worker5():
+  time.sleep(10)
+  print("Thread-3退出")
+  
+def worker2():
+   t3 = threading.Thread(target=worker5, name="Thread-3")
+   t3.start()
+   time.sleep(1)
+   print("Thread-2退出")
 
-@curry
-def add_curried(x, y, z):
-    return x + y + z
+def work_daemon():
+  while True:
+    print([i.name for i in threading.enumerate()])
+    time.sleep(1)
 
-# 使用柯里化函数
-one_plus_two = add_curried(1)
-result = one_plus_two(2)
-print(result)  # 输出 3
+t1 = threading.Thread(target=worker, name="Thread-1")
+t2 = threading.Thread(target=worker2, name="Thread-2")
+t3 = threading.Thread(target=work_daemon, name="Thread-daemon")
 
-two_plus_three = add_curried(2)(3)
-print(two_plus_three)  # 输出 5
+t1.start()
+t2.start()
+t3.setDaemon(True)#设置t3为守护线程
+t3.start()
+print("主线程运行结束")#运行结束并不意味着退出
+#输出：
+"""
+['MainThread', 'Thread-1', 'Thread-2', 'Thread-daemon']
+主线程运行结束
+Thread-2退出
+我是线程Thread-1
+['MainThread', 'Thread-1', 'Thread-daemon']
+我是线程Thread-1
+['MainThread', 'Thread-1', 'Thread-daemon']
+我是线程Thread-1
+Thread-1退出
+
+守护线程之所以叫“守护”是因为该线程通常用于为其他线程提供服务，或监控其他线程等，
+例如守护线程监测到某线程异常退出后可以重新创建该线程。因为服务于进程内的所有线程，
+所以守护线程一般是一个死循环，所有线程结束后守护线程才会自动退出循环，退出线程。
+值得注意的是线程运行结束不意味着线程退出，比如观察图二代码的图三输出，图三输出显示主线程运行结束后并没有退出，
+主线程需要其他所有非守护线程退出后才退出，主线程退出后守护线程退出
+"""
