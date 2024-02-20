d5bfb29ae75effeca814d2a7a3a09d0b0792696b
gitlG <1637885653@qq.com>
2024-02-20 14:34:57 +0800

    2023/12/08:异步网络服务器使用例子，wsgi协议

--- test.py
+++ test.py
@@ -1,29 +1,51 @@
-#装饰器wraps的使用
-import inspect
-from functools import wraps
-def test1(arg):
-    """
-    参数：arg
-    """
-    print(arg)
+#异步网络服务器使用例子，wsgi协议
+import uvicorn
+class App:
+    def __init__(self):
+        self.routes = {}
+    def route(self, path):
+        def decorator(fn):
+            self.routes[path] = fn
+            return fn
+        return decorator
 
-# 作用为是test1的元信息覆盖test2的元信息，
-# 且test1函数即是test2.__wrapped__
-# 等同于wraps(test1)(test2)
-@wraps(test1)
-def test2(arg1,arg2):
-    """
-    参数：
-    arg1: 第一个参数
-    arg2: 第二个参数
-    """
+    async def __call__(self, scope, receive, send):
+        path = scope['path']
+        handler = self.routes.get(path,None)
+        if handler is not None:
+                await handler(scope, receive, send)
+                return
+        await send({
+            'type': 'http.response.start',
+            'status': 404,
+            'headers': [(b'content-type', b'text/plain')],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': b'Not Found',
+        })
 
-test2.__wrapped__("test1")
-print(globals())
-test2_info = inspect.getdoc(test2)
-print(test2_info)
-#输出
-"""
-test1
-参数：arg
-"""
+app = App()
+@app.route('/')#"/"与该函数的绑定关系会被保存在app中的字典中
+async def handle_home(scope, receive, send):
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [(b'content-type', b'text/plain')],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello, World!',
+    })
+if __name__ == '__main__':
+    #网络请求到来后uvicorn服务器会调用app()(调用call函数)
+    uvicorn.run(app, host='localhost', port=8000)
+
+
+#bottle使用例子
+from bottle import route, run, template
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+run(host='localhost', port=8080)
