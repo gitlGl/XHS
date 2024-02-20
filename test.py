c739434d8b1e78f812e150a077707256c9f8b678
gitlG <1637885653@qq.com>
2024-02-20 14:55:51 +0800

    2024/02/15:简单的rest接口

--- test.py
+++ test.py
@@ -1,49 +1,57 @@
-#2024/02/01
-class Profiled:
-    def __init__(self, func):
-        self.get = func
-   
-    def __get__(self, instance, cls):
-       return self.get(instance)
-        
-    def __set__(self, instance, value):
-            return self.set(instance,value)
-        
-    def setter(self,func):
-        self.set = func
-        return self       
-class Person:
-    def __init__(self, name ,age):
-        self.name = name
-        self.age = age
+#2024/02/15
+import cgi
+def notfound_404(environ, start_response):
+    start_response('404 Not Found', [ ('Content-type', 'text/plain') ])
+    return [b'Not Found']
 
-    @Profiled
-    def name(self):
-        return self._name
+class PathDispatcher:
+    def __init__(self):
+        self.pathmap = { }
 
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            raise TypeError('name must be a string')
-        self._name = value
-   
-    @Profiled
-    def age(self):
-        return self._age
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+        params = cgi.FieldStorage(environ['wsgi.input'],
+                                  environ=environ)
+        method = environ['REQUEST_METHOD'].lower()
+        environ['params'] = { key: params.getvalue(key) for key in params }
+        handler = self.pathmap.get((method,path), notfound_404)
+        return handler(environ, start_response)
 
-    @age.setter
-    def age(self, value):
-        if not isinstance(value, int):
-            raise TypeError('age must be an int')
-        self._age = value
-        
-p = Person("test",13)
+    def register(self, method, path, function):
+        self.pathmap[method.lower(), path] = function
+        return function
+    
+import time
 
-print(p.age)#13
-p.age = 27
-print(p.age)#27
+_hello_resp = "hello word"
+def hello_world(environ, start_response):
+    start_response('200 OK', [ ('Content-type','text/html')])
+    params = environ['params']
+    resp = _hello_resp.format(name=params.get('name'))
+    yield resp.encode('utf-8')
 
+_localtime_resp = '''\
+<?xml version="1.0"?>
+<time>
+  <year>{t.tm_year}</year>
+  <month>{t.tm_mon}</month>
+  <day>{t.tm_mday}</day>
+  <hour>{t.tm_hour}</hour>
+  <minute>{t.tm_min}</minute>
+  <second>{t.tm_sec}</second>
+</time>'''
 
-print(p.name)#test
-p.name = "test2"
-print(p.name)#test2
+def localtime(environ, start_response):
+    start_response('200 OK', [ ('Content-type', 'application/xml') ])
+    resp = _localtime_resp.format(t=time.localtime())
+    yield resp.encode('utf-8')
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    dispatcher = PathDispatcher()
+    dispatcher.register('GET', '/hello', hello_world)
+    dispatcher.register('GET', '/localtime', localtime)
+    # Launch a basic server
+    httpd = make_server('', 8080, dispatcher)
+    print('Serving on port 8080...')
+    httpd.serve_forever()
