50416d167f52075e47f476552483670478e20f24
gitlG <1637885653@qq.com>
2024-02-20 14:38:01 +0800

    2023/12/08:base64编码例子

--- test.py
+++ test.py
@@ -1,39 +1,25 @@
-#协程例子
-import os
-import asyncio,time
-async def get_html(n,i):
-    # 模拟从网站下载页�?
-    print(f"第{n}组第{i}个任�?")
-    if n == 2:
-        await asyncio.sleep(2)
-    else:await asyncio.sleep(1)
-    print(f"第{n}组第{i}个任务完�?")
+#base64编码例子
+from base64 import b85decode,b85encode
+def split_and_merge(text, char_limit=79):
+    str_count = len(text)//char_limit
+    end_str = text[str_count*char_limit:]
+    #按指定长度将填充后的文本分割成行
+    lines = [text[i:i+char_limit] for i in range(0, len(text), char_limit)]
+    # 将所有行合并在一起，并在每行之间添加换行符
+    final_text = '\n'.join(lines)
+    return final_text
 
-async def task_list(n):
-    print(f"第{n}组任务开�?")
-    for i in range(1,3):
-        asyncio.create_task(get_html(n,i))
-    print("安排结束")
-    await asyncio.sleep(3)
 
-async def hello():
-    print("test")
-    await asyncio.sleep(3)
+#读取需要编码的二进制文件然后编码
+with open("desktop/test.zip","rb")  as f :
+    data = f.read()
+    encode_data = b85encode(data)
 
-async def hello2():
-    print("test2")
-
-async def main():
-    # 通过 asyncio.create_task 函数创建任务
-    # 调用时需要给它传递一个协程，然后返回一个任务对�?
-    lst = []
-    for i in range(1,3):
-        reuslt =  asyncio.create_task(task_list(i))
-        lst.append(reuslt)
-
-    await hello()
-    await hello2()
-    await asyncio.gather(*lst)
-    await asyncio.as_completed(lst)
-    
-asyncio.run(main(),debug= True)
+#编码后写入文本，然后记事本打开复制到本文件中，即是二进制变量DATA   
+with open("desktop/test.txt","w+") as f:
+#split_and_merge,每79个字符为一行插入换行符，
+# b85不包括换行符，不会污染文件
+    f.write(split_and_merge(encode_data.__repr__(),79))
+#把DATA二进制变量解码后写入文件就能还原文件
+with open("desktop/t.zip","wb") as f:
+    f.write(b85decode(DATA.replace(b"\n",b'')))
