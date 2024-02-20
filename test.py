a11000087c002c9d65619660ca0fc8888eeb20d4
gitlG <1637885653@qq.com>
2024-02-20 14:31:01 +0800

    2023/11/28:深拷贝与浅拷贝

--- test.py
+++ test.py
@@ -1,34 +1,17 @@
-#2023/11/29
-#装饰器使用技巧，摘抄自python工匠，
-# 不解释，有兴趣的看原书
-import time
-import functools
-class DelayFunc:
-    def __init__(self,  duration, func):
-        self.duration = duration
-        self.func = func
+#2023/11/28
+import copy
+#浅拷贝,只进行浅层拷贝，嵌套对象不拷贝
+items = [1, ['foo', 'bar'], 2, 3]
+items_copy = copy.copy(items)
+items_copy[0] = 100 
+items_copy[1].append('xxx') 
+#不影响原对象浅层对象，改变嵌套对象
+print(items)#[1, ['foo', 'bar', 'xxx'], 2, 3]
 
-    def __call__(self, *args, **kwargs):
-        print(f'Wait for {self.duration} seconds...')
-        time.sleep(self.duration)
-        return self.func(*args, **kwargs)
-
-    def eager_call(self, *args, **kwargs):
-        print('Call without delay')
-        return self.func(*args, **kwargs)
-
-def delay(duration):
-    """装饰器：推迟某个函数的执行。同时提供 .eager_call 方法立即执行
-    """
-    # 此处为了避免定义额外函数，直接使用 functools.partial 帮助构造
-    # DelayFunc 实例
-    return functools.partial(DelayFunc, duration)
-
-@delay(duration=2)
-def add(a, b):
-    return a + b
-
-# 这次调用将会延迟 2 秒
-add(1, 2)
-# 这次调用将会立即执行
-add.eager_call(1, 2)
+#深拷贝，层层拷贝
+items = [1, ['foo', 'bar'], 2, 3]
+items_copy = copy.deepcopy(items)
+items_copy[0] = 100 
+items_copy[1].append('xxx') 
+#不影响原对象，包括嵌套
+print(items)#[1, ['foo', 'bar'], 2, 3]
