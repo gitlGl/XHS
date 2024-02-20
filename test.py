3ef1ff4e235aa5ae6e4326e29bfcde79d70ecf86
gitlG <1637885653@qq.com>
2024-02-20 14:25:40 +0800

    2023/11/22:导包路径与工作路径

--- test.py
+++ test.py
@@ -1,21 +1,39 @@
-#2023/11/16
-def my_function(a, b, *, c, d):
-    print(a, b, c, d)
-
-my_function(1, 2, c=3, d=4)  # 使用关键字参数调用函数
-
-# 以下是错误的调用方式：
-#my_function(1, 2, 3, 4)  # 错误！参数c和d必须使用关键字参数传递
-#my_function(1, 2, 3, d=4)  # 错误！参数c必须使用关键字参数传递
-
-def my_function(a, b, /, c, d):
-    print(a, b, c, d)
-
-my_function(1, 2, 3, 4)  # 使用位置参数调用函数
-my_function(a = 1,b = 2, c=3, d=4)#错误，a,b必须使用位置参数传递
-
-def my_function(a, b, /, c, d, *, e, f):
-    print(a, b, c, d, e, f)
-
-my_function(1, 2, 3, 4, e=5, f=6)  # 可以使用位置参数和关键字参数
-my_function(1, 2, c=3, d=4, e=5, f=6)  # 也可以全部使用关键字参数
+ #2023/11/22
+import sys,os
+print(sys.path)
+file_path = os.path.abspath(__file__)
+path = os.path.dirname(file_path) 
+#sys.path[0]与上面两行代码等价
+print(path == sys.path[0])#输出True
+print("工作目录：",os.getcwd())
+#输出
+"""
+['c:\\Users\\Administrator\\Desktop\\XHS', 'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\python38.zip', 
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\DLLs', 
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\lib', 
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3', 
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\lib\\site-packages',
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\lib\\site-packages\\win32',
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\lib\\site-packages\\win32\\lib',
+'C:\\Users\\Administrator\\.pyenv\\pyenv-win\\versions\\3.8.3\\lib\\site-packages\\Pythonwin']
+True
+工作目录： C:\Users\Administrator\Desktop\XHS
+"""
+"""
+#path中的目录除了第一个属于用户输入以外，一是Python运行期间根据自身所在目录
+作为相对目录生成，二是根据环境变量pythonpath生成
+import sys,os
+导包过程实际完整路径是
+pyenv-win\\versions\\3.12.0\\Lib\\sys,os
+python会根据sys.path中的路径去补全完整的导包路径
+sys.path中的路径存在优先级，第一个是python 可执行文件所在路径
+如果在此路径找到需要的包则会停止搜索，否则继续寻找。找不到则报错
+可以在path中添加.pth文件，.pth文件中的路径会被加入到path中，
+但是不允许嵌套使用
+安装Python时候如果手动设置环境变量，第一个环境变量则是Python.exe
+所在路径，这个很好理解，还有一个路径是pip.exe所以在路径，其实pip.exe
+路径主要功能是存放第三方程序包所用的资源文件（二进制文件，dll文件），主要为
+二进制文件，举个例子假如你要把自己的代码推送到pypi上面供别人使用，
+而你代码中依赖一些资源文件，这样安装后你资源文件会放在pip.exe所在目录中，
+scripts目录即是此目录。
+"""
