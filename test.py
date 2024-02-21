294aad597c9e794ce9275d1db5f42bfbd387f33f
gitlG <1637885653@qq.com>
2024-02-21 15:34:19 +0800

    2024/02/21:读写csv文件

--- test.py
+++ test.py
@@ -1,34 +1,95 @@
-#2024/01/06
-#摘抄自python cookbook
-from struct import Struct
-data = [[123, 3.14, True],[123, 3.14, True]]
-format = 'if?'
-record_struct = Struct(format)
+#2024/02/21
+"""
+Symbol,Price,Date,Time,Change,Volume
+"AA",39.48,"6/11/2007","9:36am",-0.18,181800
+"AIG",71.38,"6/11/2007","9:36am",-0.15,195500
+"AXP",62.58,"6/11/2007","9:36am",-0.46,935000
+"BA",98.31,"6/11/2007","9:36am",+0.12,104800
+"C",53.08,"6/11/2007","9:36am",-0.25,360900
+"CAT",78.29,"6/11/2007","9:36am",-0.23,225400
+"""
 
-# 将二进制数据写入文件
-with open('data.b', 'wb') as f:             
-    for item in data:
-        packed_data = record_struct.pack(*item)
-        f.write(packed_data)
-
-def unpack_records(format, data):
-    record_struct = Struct(format)
-    return (record_struct.unpack_from(data, offset)
-            for offset in range(0, len(data), record_struct.size))
-    
-def read_records(format, f):
-    record_struct = Struct(format)
-    chunks = iter(lambda: f.read(record_struct.size), b'')
-    return (record_struct.unpack(chunk) for chunk in chunks)
-
-# Example
-if __name__ == '__main__':
-    with open('data.b','rb') as f:
-        for rec in read_records(format, f):
-            # Process rec
-            ...
-        data = f.read()
-        for rec in unpack_records(format, data):
-            ...
-        # Process rec
+import csv
+with open('stocks.csv') as f:
+    f_csv = csv.reader(f)
+    headers = next(f_csv)
+    for row in f_csv:
+        # Process row
+        ...
         
+#将数据读取到一个字典序列中去       
+with open('stocks.csv') as f:
+    f_csv = csv.DictReader(f)
+    for row in f_csv:
+        # process row
+        ...
+
+#将数据读写入csv
+headers = ['Symbol','Price','Date','Time','Change','Volume']
+rows = [('AA', 39.48, '6/11/2007', '9:36am', -0.18, 181800),
+         ('AIG', 71.38, '6/11/2007', '9:36am', -0.15, 195500),
+         ('AXP', 62.58, '6/11/2007', '9:36am', -0.46, 935000),
+       ]
+
+with open('stocks.csv','w') as f:
+    f_csv = csv.writer(f)
+    f_csv.writerow(headers)
+    f_csv.writerows(rows)
+    
+#将数据读写入csv   
+headers = ['Symbol', 'Price', 'Date', 'Time', 'Change', 'Volume']
+rows = [{'Symbol':'AA', 'Price':39.48, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.18, 'Volume':181800},
+        {'Symbol':'AIG', 'Price': 71.38, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.15, 'Volume': 195500},
+        {'Symbol':'AXP', 'Price': 62.58, 'Date':'6/11/2007',
+        'Time':'9:36am', 'Change':-0.46, 'Volume': 935000},
+        ]
+
+with open('stocks.csv','w') as f:
+    f_csv = csv.DictWriter(f, headers)
+    f_csv.writeheader()
+    f_csv.writerows(rows)
+    
+#不推荐   
+with open('stocks.csv') as f:
+    """使用这种方式的一个缺点就是你仍然需要去处理一些棘手的细节问题。 
+    比如，如果某些字段值被引号包围，你不得不去除这些引号。 
+    另外，如果一个被引号包围的字段碰巧含有一个逗号，
+    那么程序就会因为产生一个错误大小的行而出错。"""
+    for line in f:
+        row = line.split(',')
+            # process row
+          
+#推荐         
+with open('stock.tsv') as f:
+    f_tsv = csv.reader(f, delimiter='\t')
+    for row in f_tsv:
+        # Process row
+        ...
+        
+        
+"""
+  csv产生的数据都是字符串类型的，它不会做任何其他类型的转换。
+ 如果你需要做这样的类型转换，你必须自己手动去实现。
+ 下面是一个在CSV数据上执行其他类型转换的例子：
+ """ 
+col_types = [str, float, str, str, float, int]
+with open('stocks.csv') as f:
+    f_csv = csv.reader(f)
+    headers = next(f_csv)
+    for row in f_csv:
+        # Apply conversions to the row items
+        row = tuple(convert(value) for convert, value in zip(col_types, row))
+        ...
+              
+print('Reading as dicts with type conversion')
+field_types = [ ('Price', float),
+                ('Change', float),
+                ('Volume', int) ]
+
+with open('stocks.csv') as f:
+    for row in csv.DictReader(f):
+        row.update((key, conversion(row[key]))
+                for key, conversion in field_types)
+        print(row)
