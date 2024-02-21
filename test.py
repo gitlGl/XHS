d18c6db9ad8b1b4f504776ef6de33b1948de1e15
gitlG <1637885653@qq.com>
2024-02-21 19:12:33 +0800

    2024/02/21:读写json数据

--- test.py
+++ test.py
@@ -1,95 +1,65 @@
-#2024/02/21
-"""
-Symbol,Price,Date,Time,Change,Volume
-"AA",39.48,"6/11/2007","9:36am",-0.18,181800
-"AIG",71.38,"6/11/2007","9:36am",-0.15,195500
-"AXP",62.58,"6/11/2007","9:36am",-0.46,935000
-"BA",98.31,"6/11/2007","9:36am",+0.12,104800
-"C",53.08,"6/11/2007","9:36am",-0.25,360900
-"CAT",78.29,"6/11/2007","9:36am",-0.23,225400
-"""
+import json
+from pprint import pprint
+data = {
+    'name' : 'ACME',
+    'shares' : 100,
+    'price' : 542.23
+}
 
-import csv
-with open('stocks.csv') as f:
-    f_csv = csv.reader(f)
-    headers = next(f_csv)
-    for row in f_csv:
-        # Process row
-        ...
-        
-#将数据读取到一个字典序列中去       
-with open('stocks.csv') as f:
-    f_csv = csv.DictReader(f)
-    for row in f_csv:
-        # process row
-        ...
+json_str = json.dumps(data)
+pprint(json_str)
 
-#将数据读写入csv
-headers = ['Symbol','Price','Date','Time','Change','Volume']
-rows = [('AA', 39.48, '6/11/2007', '9:36am', -0.18, 181800),
-         ('AIG', 71.38, '6/11/2007', '9:36am', -0.15, 195500),
-         ('AXP', 62.58, '6/11/2007', '9:36am', -0.46, 935000),
-       ]
+# 把json数据文件写入文件中
+with open('data.json', 'w') as f:
+    json.dump(data, f)
 
-with open('stocks.csv','w') as f:
-    f_csv = csv.writer(f)
-    f_csv.writerow(headers)
-    f_csv.writerows(rows)
+# 从文件中读取json数据
+with open('data.json', 'r') as f:
+    data = json.load(f)
     
-#将数据读写入csv   
-headers = ['Symbol', 'Price', 'Date', 'Time', 'Change', 'Volume']
-rows = [{'Symbol':'AA', 'Price':39.48, 'Date':'6/11/2007',
-        'Time':'9:36am', 'Change':-0.18, 'Volume':181800},
-        {'Symbol':'AIG', 'Price': 71.38, 'Date':'6/11/2007',
-        'Time':'9:36am', 'Change':-0.15, 'Volume': 195500},
-        {'Symbol':'AXP', 'Price': 62.58, 'Date':'6/11/2007',
-        'Time':'9:36am', 'Change':-0.46, 'Volume': 935000},
-        ]
+#json转为字典时保持数据顺序    
+s = '{"name": "ACME", "shares": 50, "price": 490.1}'
+from collections import OrderedDict
+data = json.loads(s, object_pairs_hook=OrderedDict)
 
-with open('stocks.csv','w') as f:
-    f_csv = csv.DictWriter(f, headers)
-    f_csv.writeheader()
-    f_csv.writerows(rows)
+print(data)
+#OrderedDict([('name', 'ACME'), ('shares', 50), ('price', 490.1)])
+
+
+#对象序列化与反序列化
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+        
+def serialize_instance(obj):
+    d = { '__classname__' : type(obj).__name__ }
+    d.update(vars(obj))
+    return d
+
+
+classes = {
+    'Point' : Point
+}
+
+def unserialize_object(d):
+    clsname = d.pop('__classname__', None)
+    if clsname:
+        cls = classes[clsname]
+        obj = cls.__new__(cls) # Make instance without calling __init__
+        for key, value in d.items():
+            setattr(obj, key, value)
+        return obj
+    else:
+        return d
     
-#不推荐   
-with open('stocks.csv') as f:
-    """使用这种方式的一个缺点就是你仍然需要去处理一些棘手的细节问题。 
-    比如，如果某些字段值被引号包围，你不得不去除这些引号。 
-    另外，如果一个被引号包围的字段碰巧含有一个逗号，
-    那么程序就会因为产生一个错误大小的行而出错。"""
-    for line in f:
-        row = line.split(',')
-            # process row
-          
-#推荐         
-with open('stock.tsv') as f:
-    f_tsv = csv.reader(f, delimiter='\t')
-    for row in f_tsv:
-        # Process row
-        ...
-        
-        
-"""
-  csv产生的数据都是字符串类型的，它不会做任何其他类型的转换。
- 如果你需要做这样的类型转换，你必须自己手动去实现。
- 下面是一个在CSV数据上执行其他类型转换的例子：
- """ 
-col_types = [str, float, str, str, float, int]
-with open('stocks.csv') as f:
-    f_csv = csv.reader(f)
-    headers = next(f_csv)
-    for row in f_csv:
-        # Apply conversions to the row items
-        row = tuple(convert(value) for convert, value in zip(col_types, row))
-        ...
-              
-print('Reading as dicts with type conversion')
-field_types = [ ('Price', float),
-                ('Change', float),
-                ('Volume', int) ]
+    
+    
+p = Point(2,3)
+s = json.dumps(p, default=serialize_instance)
+print(s)#'{"__classname__": "Point", "y": 3, "x": 2}'
 
-with open('stocks.csv') as f:
-    for row in csv.DictReader(f):
-        row.update((key, conversion(row[key]))
-                for key, conversion in field_types)
-        print(row)
+a = json.loads(s, object_hook=unserialize_object)
+print(a)#<__main__.Point object at 0x1017577d0>
+
