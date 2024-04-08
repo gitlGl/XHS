47ee02ebb561979afd424b36f265415a474ba5a7
gitlG <1637885653@qq.com>
2024-04-08 08:01:11 +0800

    2024/04/08:代理对象，使用__getattr__， __setattr__与 __getattribute__

--- test.py
+++ test.py
@@ -1,32 +1,52 @@
+class Proxy:
+    def __init__(self, obj):
+        self._obj = obj
+        self._name = 9
+       
+    # 当实例对象找不到属性时调用该函数
+    def __getattr__(self, name):
+        print("test3")
+        
+        return getattr(self._obj, name)
+    # 每次设置属性时都会调用这个方法，不管属性是否已经存在。
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            print(9)
+            #调用父类的__setattr__函数
+            super(Proxy,self).__setattr__(name, value)
+            
+        else:
+            setattr(self._obj, name, value)
+    
+    #读时一定先调用此属性，
+    #如果属性存在，它会直接返回；如果实例没有该属性，
+    # 那么会检测我们是否定义了 __getattr__，定义了则执行，
+    # 没定义则抛出 AttributeError。我们将这两个方        a
+    def __getattribute__(self, __name: str):
+        if __name in  ["age","name"]:
+            raise ValueError("禁止访问")
+        else:
+            print("test")
+           
+            return super(Proxy,self).__getattribute__(__name)
+class A():
+    pass
+   # nam = 8     
+p = Proxy(A())
+p._name = "test"
+p.nam
+#p.xx
+
 """
-关于__slots__ 的一个常见误区是它可以作为一个封装工具来防止
-用户给实例增加新的属性。 
-尽管使用slots可以达到这样的目的，但是这个并不是它的初衷。
-__slots__ 更多的是用来作为一个内存优化工具。
+抱歉最近心情极差，忘了更新。
+
+整个执行顺序：
+1.创建对象后先执行构造函数， self._obj = obj语句第一次触发
+ __setattr__函数输出“9”，接着执行 self._name = 9
+语句第二次触发__setattr__再次输出“9”
+接着执行p._name = "test"，第三次触发__setattr__输出“9”
+2.执行p.nam,触发 __getattribute__输出“test”，因为没有定义nam属性
+会__getattr__，所以输出test3，其中的self._obj语句会再次触发__getattr__
+然后输出test，因为obj上并没有该属性，所以最终报错。
+
     """
-class Date:
-    __slots__ = ['year', 'month', 'day']
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-                
-"""
-当你定义 __slots__ 后，Python就会为实例使用一种更加紧凑的内部表示。 
-过一个很小的固定大小的数组来构建，而不是为每个实例定义一个字典，
-这跟元组或列表很类似。
-在 __slots__ 中列出的属性名在内部被映射到这个数组的指定小标上。
-使用slots一个不好的地方就是我们不能再给实例添加新的属性了，
-只能使用在 __slots__ 中定义的那些属性名。
-"""
-
-
-"""
-使用slots后节省的内存会跟存储属性的数量和类型有关。 
-不过，一般来讲，使用到的内存总量和将数据存储在一个元组中差不多。 
-为了给你一个直观认识，假设你不使用slots直接存储一个Date实例， 在64位的Python上面要占用428字节，
-而如果使用了slots，内存占用下降到156字节。 如果程序中需要同时创建大量的日期实例，那么这个就能极大的减小内存使用量了。
-尽管slots看上去是一个很有用的特性，很多时候你还是得减少对它的使用冲动。 
-Python的很多特性都依赖于普通的基于字典的实现。 另外，定义了slots后的类不再支持一些普通类特性了，比如多继承。 
-大多数情况下，你应该只在那些经常被使用到的用作数据结构的类上定义slots (比如在程序中需要创建某个类的几百万个实例对象)。
-        """
