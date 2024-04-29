cc2c6abf18190c84f2fc8c9df86286694a90928d
gitlG <1637885653@qq.com>
2024-04-29 16:51:23 +0800

    2024/04/29:自定义zip，chain，enumerate函数

--- test.py
+++ test.py
@@ -1,6 +1,34 @@
-#python setup.py bdist_wheel --no DLIB_USE_CUDA
-"""
-    编译dlib，19.24.0及以上版本不支持3.11以上python版本
-    报错pyblind11编译错误
+numbers = [1, 2, 3]
+letters = ['a', 'b', 'c']
+
+def custom_enumerate(iterable, start=0):
+    index = start
+    for item in iterable:
+        yield index, item
+        index += 1
+
+for index, letter in custom_enumerate(letters, start=1):
+    print(index, letter)
+
+def custom_zip(*iterables):
+    # 获取所有可迭代对象的迭代器
+    iterators = [iter(iterable) for iterable in iterables]
+    while True:
+        try:
+            items = [next(iterator) for iterator in iterators]
+        except StopIteration:
+            # 如果任何一个迭代器耗尽，则停止迭代
+            return "StopIteration"
+        else:
+            yield tuple(items)
+
+for item in custom_zip(numbers, letters):
+    print(item)
     
-"""
+def custom_chain(*iterables):
+    for iterable in iterables:
+        for item in iterable:
+            yield item
+
+for item in  custom_chain(numbers, letters):
+    print(item)
